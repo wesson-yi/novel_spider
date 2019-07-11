3700667f52f05cb68a82b923be9cc0455a2d5dae
wesson-yi <wesson_yi@163.com>
2019-07-11 23:05:12 +0800

    View Home

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # get 'static_pages/home'
   # get '/static_pages/home'
   # get '/home'
+
   get '/help',    to: 'static_pages#help' # , as: 'helf'
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
